k9mail/k9mail.app

commit dd6b3ab25165277cd9e0497216945fecb793d2d1

First push for new site design

Gemfile
@@ -1,3 +1,14 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'github-pages'
+gem "jekyll-github-metadata"
+gem "jekyll-paginate"
+gem "jekyll-compose"
+gem "github-pages", group: :jekyll_plugins
+
+install_if -> { RUBY_PLATFORM =~ %r!mingw|mswin|java! } do
+  gem "tzinfo", "~> 1.2"
+  gem "tzinfo-data"
+end
+
+gem "wdm", "~> 0.1.1", :install_if => Gem.win_platform?
+

_config.yml
@@ -1,9 +1,28 @@
-url: https://k9mail.app
 title: K-9 Mail
+email: info@k9mail.app
 description: "K-9 Mail is an open source email client focused on making it easy to chew through large volumes of email"
+url: "https://anxh3l0.github.io/k9mail.github.io"
+github_username: k9mail
+github: k9mail
 
-markdown_ext: "markdown,mkdown,mkdn,mkd,md"
-markdown:    kramdown
+# Build settings
+plugins:
+  - jekyll-github-metadata
+  - jekyll-paginate
+  - jekyll-compose
 
-gems:
-  - jekyll-feed
+collections:
+  docs:
+    output: "true"
+
+exclude:
+   - .sass-cache/
+   - .jekyll-cache/
+   - gemfiles/
+   - Gemfile
+   - Gemfile.lock
+   - node_modules/
+   - vendor/bundle/
+   - vendor/cache/
+   - vendor/gems/
+   - vendor/ruby/

_includes/head.html
@@ -1,0 +1,35 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{% if page.title %}{{ page.title }}{% else %}{{ site.title }}{% endif %}</title>
+    <meta name="description" content="{% if page.excerpt %}{{ page.excerpt | strip_html | strip_newlines | truncate: 160 }}{% else %}{{ site.description }}{% endif %}">
+    <!-- Social: Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="{% if page.title %}{{ page.title }}{% else %}{{ site.title }}{% endif %}">
+    <meta name="twitter:description" content="{% if page.description %}{{ page.description | strip_html | strip_newlines | truncate: 160 }}{% else %}{{ site.description }}{% endif %}"> {% if page.image %}
+    <meta property="twitter:image:src" content="{{ page.image }}"> {% else %}
+    <meta property="twitter:image:src" content="{{ "/assets/img/preview.png" | prepend: site.url }}"> {% endif %}
+
+    <!-- Social: Facebook / Open Graph -->
+    <meta property="og:url" content="{{ page.url | replace:'index.html','' | prepend: site.baseurl | prepend: site.url }}">
+    <meta property="og:title" content="{% if page.title %}{{ page.title }}{% else %}{{ site.title }}{% endif %}"> {% if page.image %}
+    <meta property="og:image" content="{{ page.image }}"> {% else %}
+    <meta property="og:image" content="{{ "/assets/img/preview.png" | prepend: site.url }}"> {% endif %}
+
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:description" content="{% if page.description %}{{ page.description | strip_html | strip_newlines | truncate: 160 }}{% else %}{{ site.description }}{% endif %}">
+    <meta property="og:site_name" content="{{ site.title }}">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ "/favicon.ico" | prepend: site.url }}" type="image/x-icon"/>
+    <link rel="stylesheet" href="{{ "/assets/css/main.css" | prepend: site.url }}">
+    <link rel="stylesheet" href="{{ "/assets/css/all.css" | prepend: site.url }}">
+    <link rel="stylesheet" href="{{ "/assets/css/inter.css" | prepend: site.url }}">
+    <link rel="canonical" href="{{ page.url | replace:'index.html','' | prepend: site.baseurl | prepend: site.url }}">
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="{{ "/sitemap.xml" | prepend: site.url }}" />
+    <!-- JavaScript -->
+    <script src="{{ "/assets/js/jquery-3.4.1.slim.min.js" | prepend: site.url }}"></script>
+    <script src="{{ "/assets/js/bootstrap.bundle.min.js" | prepend: site.url }}"></script>
+    <script src="{{ "/assets/js/swiper.min.js" | prepend: site.url }}"></script>
+    <script src="{{ "/assets/js/search.min.js" | prepend: site.url }}"></script>
+</head>

commit c5ffc7b651b51360f4e9b905a002d64f36f70d11

Re-add atom feed
Initially added in 2747eeb174a595bb27efb4bbbfd33870e045ebdc (#28),
this was a regression from dd6b3ab25165277cd9e0497216945fecb793d2d1.

Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 gem "jekyll-github-metadata"
 gem "jekyll-paginate"
 gem "jekyll-compose"
+gem "jekyll-feed"
 gem "github-pages", group: :jekyll_plugins
 
 install_if -> { RUBY_PLATFORM =~ %r!mingw|mswin|java! } do

_config.yml
@@ -8,6 +8,7 @@ plugins:
   - jekyll-github-metadata
   - jekyll-paginate
   - jekyll-compose
+  - jekyll-feed
 
 collections:
   documentation:

_includes/head.html
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{% if page.title %}{{ page.title }}{% else %}{{ site.title }}{% endif %}</title>
     <meta name="description" content="{% if page.excerpt %}{{ page.excerpt | strip_html | strip_newlines | truncate: 160 }}{% else %}{{ site.description }}{% endif %}">
+    {% feed_meta %}
     <!-- Social: Twitter -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="{% if page.title %}{{ page.title }}{% else %}{{ site.title }}{% endif %}">