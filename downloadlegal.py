ff52255e8295db1a6f344a53f7abd227fcd4a135
Philipp Kewisch <mozilla@kewis.ch>
2024-11-22 09:24:08 +0100

    Download Privacy Policy from Thunderbird

---
+++ downloadlegal.py
@@ -0,0 +1,32 @@
+import re
+import sys
+import time
+import urllib.request as request
+
+# Download the Thunderbird Privacy Policy and fit it into our layout
+
+THUNDERBIRD_PRIVACY_POLICY_URL = "https://raw.githubusercontent.com/mozilla/legal-docs/main/en/thunderbird_privacy_policy.md"
+
+JEKYLL_HEADER = """
+---
+layout: doc
+title: K-9 Mail Privacy Notice
+---
+""".strip()
+
+
+def main():
+    url = f"{THUNDERBIRD_PRIVACY_POLICY_URL}?token={int(time.time())}"
+    with request.urlopen(url) as response:
+        contents = (
+            response.read()
+            .decode("utf-8")
+            .replace("Thunderbird Privacy Notice", "K-9 Mail Privacy Notice")
+        )
+
+    with open("privacy.md", "w") as file:
+        file.write(f"{JEKYLL_HEADER}\n\n{contents}")
+
+
+if __name__ == "__main__":
+    main()
